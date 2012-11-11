01483f9e8fcd22ff43f54f33d5d80ad0c5d9ad2b
Mark Cox <markcox80@gmail.com>
2012-11-11 21:56:43 +1000

    Extended magic header search tests.

--- tests/packet.lisp
+++ tests/packet.lisp
@@ -5,13 +5,15 @@
 				   :element-type '(unsigned-byte 8)
 				   :initial-contents '(0 0 255 255)))
 	 (fn (basic-binary-packet::make-magic-header-search-function magic-header)))
-    (flexi-streams:with-input-from-sequence (in (make-array 9
+    (flexi-streams:with-input-from-sequence (in (make-array 10
 							    :element-type '(unsigned-byte 8)
-							    :initial-contents '(0 0 255 128 0 0 255 255 12)))
+							    :initial-contents '(0 0 255 128 0 0 255 255 12 128)))
       (dotimes (i 7)
 	(assert-false (funcall fn in)))
       (assert-true (funcall fn in))
-      (assert-equal 12 (read-byte in)))))
+      (assert-equal 12 (read-byte in))
+      (assert-false (funcall fn in))
+      (assert-error 'error (funcall fn in)))))
 
 (defun sequence-equal-p (a b &key (test #'eql))
   (and (= (length a)
