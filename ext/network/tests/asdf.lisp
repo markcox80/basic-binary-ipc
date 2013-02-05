0cdad179e1afd1f302328ab56abf1df0604a9b25
Mark Cox <markcox80@gmail.com>
2013-02-05 20:19:50 +1000

    Removed Network extension from repository.

--- ext/network/tests/asdf.lisp
+++
@@ -1,4 +0,0 @@
-(in-package "BASIC-BINARY-PACKET.NETWORK.TESTS")
-
-(defmethod asdf:perform ((operation asdf:test-op) (component (eql (asdf:find-system "basic-binary-packet-network-tests"))))
-  (lisp-unit:run-tests :all "BASIC-BINARY-PACKET.NETWORK.TESTS"))
