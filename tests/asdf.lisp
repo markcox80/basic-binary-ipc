2f96601b89c9edf4fdd83241255c13a176a27305
Mark Cox <markcox80@gmail.com>
2013-03-17 07:52:31 +1000

    Removed BASIC-BINARY-PACKET system.

--- tests/asdf.lisp
+++
@@ -1,4 +0,0 @@
-(in-package "BASIC-BINARY-PACKET.TESTS")
-
-(defmethod asdf:perform ((op asdf:test-op) (component (eql (asdf:find-system "basic-binary-packet-tests"))))
-  (lisp-unit:run-tests :all "BASIC-BINARY-PACKET.TESTS"))
