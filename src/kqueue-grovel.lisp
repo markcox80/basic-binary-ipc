27908161e58c8d9e415894f450a48f26834a88c6
Mark Cox <markcox80@gmail.com>
2013-07-11 20:58:31 +1000

    Adapted kqueue-grovel for FREEBSD.

--- src/kqueue-grovel.lisp
+++ src/kqueue-grovel.lisp
@@ -4,6 +4,22 @@
 (include "sys/event.h")
 (include "sys/time.h")
 
+#+freebsd
+(progn
+  (ctype uintptr-t "uintptr_t")
+  (ctype intptr-t "intptr_t")
+  (ctype u-short "u_short")
+  (ctype u-int "u_int")
+
+  (cstruct kevent "struct kevent"
+    (ident "ident" :type uintptr-t)
+    (filter "filter" :type :short)
+    (flags "flags" :type u-short)
+    (fflags "fflags" :type u-int)
+    (data "data" :type intptr-t)
+    (udata "udata" :type :pointer)))
+
+#+darwin
 (cstruct kevent64-s "struct kevent64_s"
   (ident "ident" :type :uint64)
   (filter "filter" :type :int16)
@@ -19,7 +35,9 @@
   (tv-sec "tv_sec" :type time-t)
   (tv-nsec "tv_nsec" :type :long))
 
-(constantenum (kevent-flags :base-type :uint16)
+(constantenum (kevent-flags :base-type
+			    #+darwin :uint16
+			    #+freebsd u-short)
   ((:ev-add "EV_ADD"))
   ((:ev-enable "EV_ENABLE"))
   ((:ev-disable "EV_DISABLE"))
@@ -28,6 +46,8 @@
   ((:ev-eof "EV_EOF"))
   ((:ev-error "EV_ERROR")))
 
-(constantenum (kevent-filters :base-type :int16)
+(constantenum (kevent-filters :base-type
+			      #+darwin :int16
+			      #+freebsd :short)
   ((:evfilt-read "EVFILT_READ"))
   ((:evfilt-write "EVFILT_WRITE")))
