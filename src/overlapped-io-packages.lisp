996b9b995a1cf951dc96627da18594e5fc2a61c9
Mark Cox <markcox80@gmail.com>
2013-09-17 21:15:00 +1000

    Exported symbols from the .OVERLAPPED-IO package.

--- src/overlapped-io-packages.lisp
+++ src/overlapped-io-packages.lisp
@@ -2,4 +2,29 @@
   (:use "COMMON-LISP")
   (:import-from "BASIC-BINARY-IPC"
 		#:define-check-system-call
-		#:define-system-call))
+		#:define-system-call)
+  ;; Requests
+  (:export #:request
+	   #:free-request
+	   #:invalidp
+	   #:waitingp
+	   #:completedp
+
+	   #:with-request
+	   #:do-with-request)
+
+  ;; Synchronising
+  (:export #:wait-for-single-object)
+
+  ;; Generic handle stuff
+  (:export #:close-handle
+	   #:with-handle
+	   #:do-with-handle
+	   #:cancel-all-io)
+
+  ;; Named Pipes
+  (:export #:connect-to-named-pipe
+
+	   ;; Servers
+	   #:make-named-pipe-server
+	   #:connect-named-pipe))
