ebf50cda1e48fda6f5540cffe22e5af04e7c9f2c
Mark Cox <markcox80@gmail.com>
2013-11-15 20:59:08 +1000

    Obtain the GUID for WSAID_CONNECTEX.

--- src/overlapped-io.c
+++ src/overlapped-io.c
@@ -4,6 +4,7 @@
 
 #define STOLEN_WSAID_GETACCEPTEXSOCKADDRS {0xb5367df2,0xcbac,0x11cf,{0x95,0xca,0x00,0x80,0x5f,0x48,0xa1,0x92}}
 #define STOLEN_WSAID_ACCEPTEX {0xb5367df1,0xcbac,0x11cf,{0x95,0xca,0x00,0x80,0x5f,0x48,0xa1,0x92}}
+#define STOLEN_WSAID_CONNECTEX {0x25a207b9,0xddf3,0x4660,{0x8e,0xe9,0x76,0xe5,0x8c,0x74,0x06,0x3e}}
 
 #ifndef WSAID_GETACCEPTEXSOCKADDRS
 #define WSAID_GETACCEPTEXSOCKADDRS STOLEN_WSAID_GETACCEPTEXSOCKADDRS
@@ -11,6 +12,10 @@
 
 #ifndef WSAID_ACCEPTEX
 #define WSAID_ACCEPTEX STOLEN_WSAID_ACCEPTEX
+#endif
+
+#ifndef WSAID_CONNECTEX
+#define WSAID_CONNECTEX STOLEN_WSAID_CONNECTEX
 #endif
 
 void
@@ -42,6 +47,12 @@ main(int argc, char **argv)
     GUID guid = WSAID_ACCEPTEX;
     output_guid("WSAID_ACCEPTEX", guid);
   }
+
+  {
+    GUID guid = WSAID_CONNECTEX;
+    output_guid("WSAID_CONNECTEX", guid);
+  }
+
 
   return 0;
 }
