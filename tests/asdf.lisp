1356b172e3a5aa7aa46ba50582a222a1e932b93b
Mark Cox <markcox80@gmail.com>
2013-12-05 21:07:35 +1000

    Display the name of the package before running the package's tests.

--- tests/asdf.lisp
+++ tests/asdf.lisp
@@ -3,6 +3,7 @@
 (defmethod asdf:perform ((operation asdf:test-op) (component (eql (asdf:find-system "basic-binary-ipc-tests"))))
   (dolist (pkg (list "BASIC-BINARY-IPC.TESTS"
 		     #+windows "BASIC-BINARY-IPC.OVERLAPPED-IO.TESTS"))
+    (format t "~&;;;; Running tests in package ~A~%" pkg)
     (let ((report (lisp-unit:run-tests :all pkg)))
       (print-failures report)
       (print-errors report))))
